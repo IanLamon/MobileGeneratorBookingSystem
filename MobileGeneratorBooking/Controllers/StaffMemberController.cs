2b09110ee54fd28cf550f05fc706a8fce0442e21
Ian Lamon <ianlamon@outlook.com>
2016-06-21 21:59:08 +0100

    Adding Staff Member model, controller and views to MVC Application

---
+++ MobileGeneratorBooking/Controllers/StaffMemberController.cs
@@ -0,0 +1,143 @@
+﻿using MobileGeneratorBooking.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MobileStaffMemberBooking.Controllers
+{
+    public class StaffMemberController : Controller
+    {
+        HttpClient client;
+        //The URL of the WEB API Service
+        string url = "http://bookingservice.azurewebsites.net/api/staffmembers";
+        
+        //The HttpClient Class, this will be used for performing 
+        //HTTP Operations, GET, POST, PUT, DELETE
+        //Set the base address and the Header Formatter
+        public StaffMemberController()
+        {
+            client = new HttpClient();  
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json")); 
+        }
+
+
+        //*********************************************************************//
+        // GET All: StaffMember
+        public async Task<ActionResult> Index()
+        {
+            HttpResponseMessage responseMessage = await client.GetAsync(url);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+
+                var StaffMembers = JsonConvert.DeserializeObject<List<StaffMember>>(responseData);
+
+                return View(StaffMembers);
+            }
+            return View("Error");
+        }
+
+
+        //*********************************************************************//
+        // Get One: StaffMember
+        public async Task<ActionResult> Details(int id)
+        {
+            HttpResponseMessage responseMessage = await client.GetAsync(url + "/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+
+                var staffMember = JsonConvert.DeserializeObject<StaffMember>(responseData);
+
+                return View(staffMember);
+            }
+            return View("Error");
+        }
+
+
+        //*********************************************************************//
+        //Edit: StaffMember
+        public async Task<ActionResult> Edit(int id)
+        {
+            HttpResponseMessage responseMessage = await client.GetAsync(url + "/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+
+                var staffMember = JsonConvert.DeserializeObject<StaffMember>(responseData);
+
+                return View(staffMember);
+            }
+            return View("Error");
+        }
+
+        //The PUT Method
+        [HttpPost]
+        public async Task<ActionResult> Edit(int id, StaffMember staffMember)
+        {
+            HttpResponseMessage responseMessage = await client.PutAsJsonAsync(url + "/" + id, staffMember);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Error");
+        }
+
+
+        //*********************************************************************//
+        //Create: StaffMember
+        public ActionResult Create()
+        {
+            return View(new StaffMember());
+        }
+
+        //The Post method
+        [HttpPost]
+        public async Task<ActionResult> Create(StaffMember staffMember)
+        {
+            HttpResponseMessage responseMessage = await client.PostAsJsonAsync(url, staffMember);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Error");
+        }
+
+
+        //*********************************************************************//
+        //Delete: StaffMember
+        public async Task<ActionResult> Delete(int id)
+        {
+            HttpResponseMessage responseMessage = await client.GetAsync(url + "/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+
+                var staffMember = JsonConvert.DeserializeObject<StaffMember>(responseData);
+
+                return View(staffMember);
+            }
+            return View("Error");
+        }
+
+        //The DELETE method
+        [HttpPost]
+        public async Task<ActionResult> Delete(int id, StaffMember staffMember)
+        {
+            HttpResponseMessage responseMessage = await client.DeleteAsync(url + "/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Error");
+        }
+    }
+}
---
+++ MobileGeneratorBooking/Models/StaffMember.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MobileGeneratorBooking.Models
+{
+    public class StaffMember
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+
+        [Required]
+        [DisplayName("Date of Birth")]
+        public DateTime DOB { get; set; }
+
+        [Required]
+        [DisplayName("PPS Number")]
+        public string PPSNumber { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Telephone { get; set; }
+
+        [Required]
+        [DisplayName("Business Unit")]
+        public string BusinessUnit { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+
+        [Required]
+        public int Level { get; set; }
+
+        [Required]
+        [DisplayName("Start Date")]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public bool Operator { get; set; }
+
+        [Required]
+        [DisplayName("Traffic Manager")]
+        public bool Traffic { get; set; }
+    }
+}
