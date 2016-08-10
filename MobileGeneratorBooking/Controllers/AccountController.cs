799a927f4f821b9c933e285b13ef7647d618b819
Ian Lamon <ianlamon@outlook.com>
2016-08-10 11:38:46 +0100

    Adding User Roles

--- MobileGeneratorBooking/Controllers/AccountController.cs
+++ MobileGeneratorBooking/Controllers/AccountController.cs
@@ -152,7 +152,7 @@ namespace MobileGeneratorBooking.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, FirstName = model.FirstName, Surname = model.Surname, Role = model.Role };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
